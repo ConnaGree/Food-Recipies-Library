5a90b1f5c55ec128b882c3271ee23975e5ea37b4
Zerubabel Yonas <93067387+ConnaGree@users.noreply.github.com>
2023-02-05 20:31:23 +0530

    Delete components directory

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Popular } from "./Popular";
-export { default as Vegetarian } from "./Vegetarian";
-export { default as Category } from "./Category";
-export { default as Search } from "./Search";
